f6c5b060cd7cae06de36284f506edd8d8dd64037
Miroslav Purkrabek <mira.purkrabek@gmail.com>
2022-04-08 11:31:37 +0200

    visualization with plotly, colors wrong

--- visualize_embeddings.py
+++ visualize_embeddings.py
@@ -6,8 +6,10 @@ from sklearn.manifold import TSNE
 import plotly
 import plotly.graph_objs as go
 
-EMBEDDINGS_PATH="data/embeddings/custom_market_dataset_unique/query/embeddings.npy"
-PATHS_PATH="data/embeddings/custom_market_dataset_unique/query/paths.npy"
+# EMBEDDINGS_PATH="data/embeddings/custom_market_dataset_unique/query/embeddings.npy"
+# PATHS_PATH="data/embeddings/custom_market_dataset_unique/query/paths.npy"
+EMBEDDINGS_PATH="data/embeddings/custom_market_dataset_unique/bounding_box_test/embeddings.npy"
+PATHS_PATH="data/embeddings/custom_market_dataset_unique/bounding_box_test/paths.npy"
 
 def load_embeddings():
     return np.load(EMBEDDINGS_PATH), np.load(PATHS_PATH)
@@ -18,17 +20,21 @@ def extract_ids_from_paths(paths):
         ids[pi] = os.path.basename(pth).split("_")[0]
     return ids
 
-def id_to_RGB(n):
+def id_to_RGB(n, normalized=False):
     n = ((n ^ n >> 15) * 2246822519) & 0xffffffff
     n = ((n ^ n >> 13) * 3266489917) & 0xffffffff
     n = (n ^ n >> 16) >> 8
-    return list(map( lambda x: x/255 , n.to_bytes(3, 'big') ))
+    
+    if normalized:
+        return list(map( lambda x: x/255 , n.to_bytes(3, 'big') ))
+    else:
+        return list(n.to_bytes(3, 'big') )
 
 def display_tsne_scatterplot(
     embeddings,
     ids=None,
     method="dummy",
-    show_points=20,
+    show_points=1000,
     num_dims=2,
     perplexity = 5,
     learning_rate = 500,
@@ -62,28 +68,63 @@ def display_tsne_scatterplot(
 
     print(compact_embeddings.shape)
 
+    data = []
+
     if num_dims == 2:
         for pi, pt in enumerate(compact_embeddings):
             if ids is None:
                 color = "blue"
             else:
-                color = [id_to_RGB(int(ids[pi]))] 
+                color = id_to_RGB(int(ids[pi]))
 
-            plt.scatter(
-                pt[0],
-                pt[1],
-                c = color,
-                alpha=0.5
+            trace = go.Scatter(
+                    x = [pt[0]], 
+                    y = [pt[1]],  
+                    text = "{:d}".format(int(ids[pi])),
+                    name = "ID {:d}".format(int(ids[pi])),
+                    textposition = "top center",
+                    textfont_size = 20,
+                    mode = 'markers+text',
+                    marker = {
+                        'size': 10,
+                        'opacity': 0.8,
+                        'color': 2
+                    }
             )
+            data.append(trace)
 
-    plt.savefig("tmp_img.jpg")
+    layout = go.Layout(
+        margin = {'l': 0, 'r': 0, 'b': 0, 't': 0},
+        showlegend=True,
+        legend=dict(
+            x=1,
+            y=0.5,
+            font=dict(
+                family="Courier New",
+                size=25,
+                color="black"
+        )),
+        font = dict(
+            family = " Courier New ",
+            size = 15),
+        autosize = False,
+        width = 1000,
+        height = 1000
+        )
+
+    plot_figure = go.Figure(data = data, layout = layout)
+    plot_figure.write_html(
+        "tmp_img.html",
+        full_html=False,
+        include_plotlyjs='cnd',
+    )
 
 
 if __name__=="__main__":
 
     print("Loading embeddings")
     embeddings, paths = load_embeddings()
-    print("Embeddings loaded")
+    print("Embeddings loaded - {}".format(embeddings.shape))
 
     ids = extract_ids_from_paths(paths)
 
