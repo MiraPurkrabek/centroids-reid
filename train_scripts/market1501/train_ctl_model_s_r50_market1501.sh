06361b5a0437c021e3779331d7101ca39137ef31
Miroslav Purkrabek <mira.purkrabek@gmail.com>
2022-04-08 09:10:33 +0200

    edited train script

--- train_scripts/market1501/train_ctl_model_s_r50_market1501.sh
+++ train_scripts/market1501/train_ctl_model_s_r50_market1501.sh
@@ -1,11 +1,11 @@
-python train_ctl_model.py \
+python3 train_ctl_model.py \
 --config_file="configs/256_resnet50.yml" \
 GPU_IDS [0] \
-DATASETS.NAMES 'market1501' \
-DATASETS.ROOT_DIR '/data/mwieczorek/' \
-SOLVER.IMS_PER_BATCH 16 \
+DATASETS.NAMES 'custom_market_dataset' \
+DATASETS.ROOT_DIR './data/' \
+SOLVER.IMS_PER_BATCH 32 \
 TEST.IMS_PER_BATCH 128 \
 SOLVER.BASE_LR 0.00035 \
-OUTPUT_DIR './logs/market1501/256_resnet50' \
+OUTPUT_DIR './logs/custom_market_dataset_unique/256_resnet50/07_04_2022' \
 DATALOADER.USE_RESAMPLING False \
 USE_MIXED_PRECISION False
